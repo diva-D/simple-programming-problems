7295798c2678ff53e8472657bb1b0efd4311f2fb
diva-D <davidgentile@gmail.com>
2018-07-26 22:13:59 -0400

    Solved elementary 1

--- Elementary/elementary-1.js
+++ Elementary/elementary-1.js
@@ -1,1 +1,9 @@
 // Write a program that prints ‘Hello World’ to the screen.
+
+// ES5
+function printHello() {
+    console.log("Hello World");
+}
+
+// ES6
+let printHello = () => console.log("Hello World");
