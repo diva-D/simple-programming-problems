af55cc5516e765cc4f6bef4a10e24861f524174d
diva-D <davidgentile@gmail.com>
2018-07-26 22:21:28 -0400

    Answer for p2

--- Elementary/elementary-2.js
+++ Elementary/elementary-2.js
@@ -1,1 +1,13 @@
 // Write a program that asks the user for their name and greets them with their name.
+
+// ES5
+function greetUser() {
+    var userName = prompt("What is your name?");
+    alert("Hi, " + userName);
+}
+
+// ES6
+let greetUser2 = () => {
+    let userName2 = prompt("What is your name?");
+    alert(`Hi ${userName2}`);
+};
